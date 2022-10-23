42b9f966ac78fedf0922a550335550d420a764b7
Glen <glencbz@gmail.com>
2022-10-23 16:08:26 -0700

    strapi: populate simple data from Strapi

--- strapi/glen-frontend/src/App.tsx
+++ strapi/glen-frontend/src/App.tsx
@@ -1,22 +1,48 @@
-import { default as React } from 'react';
-import './App.css';
+ import './App.css';
+import { useCallback, useState, default as React } from 'react';
+import { default as axios } from 'axios';
+
+// Default port for Strapi. Configure as needed.
+const API_ROOT = 'http://localhost:1337/api';
+// Routes. Comes from our Strapi config.
+const ROUTES = {
+  LAW_SOC: '/law-soc',
+}
+
+// Strapi wraps each model in a "data". Inside which, the things we care about
+// are in "attributes".
+interface StrapiModelResponse<T> {
+  data: {
+    attributes: T
+  }
+}
+
+interface LawSocView {
+  title: string;
+  subtitle: string;
+}
 
 function App() {
+  const [title, setTitle] = useState('Placeholder');
+  const [subtitle, setSubtitle] = useState('Also a placeholder');
+
+  const getBackendData = useCallback(async () => {
+    const response = await axios
+      .get<StrapiModelResponse<LawSocView>>(API_ROOT + ROUTES.LAW_SOC);
+    const {
+      title: newTitle,
+      subtitle: newSubtitle
+    } = response.data.data.attributes;
+
+    setTitle(newTitle);
+    setSubtitle(newSubtitle);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{title}</h1>
+      <h2>{subtitle}</h2>
+      <button onClick={getBackendData}>Get some backend data!!</button>
     </div>
   );
 }
